fca02f538d9942ac7dd5e9bd18ffbb72027596a3
dongja94 <dongja94@gmail.com>
2016-01-12 11:55:03 +0900

    selector

--- app/src/main/java/com/example/dongja94/samplebasicwidget/MainActivity.java
+++ app/src/main/java/com/example/dongja94/samplebasicwidget/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dongja94.samplebasicwidget;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,10 +25,10 @@ public class MainActivity extends AppCompatActivity {
 //                String message = getResources().getString(R.string.text_test);
 //                messageView.setText(message);
 //                messageView.setText(R.string.text_test);
-                count = 5;
-                messageView.setText(""+count);
-//                String message = getResources().getString(R.string.text_html);
-//                messageView.setText(Html.fromHtml(message));
+//                count = 5;
+//                messageView.setText(""+count);
+                String message = getResources().getString(R.string.text_html);
+                messageView.setText(Html.fromHtml(message));
             }
         });
     }
