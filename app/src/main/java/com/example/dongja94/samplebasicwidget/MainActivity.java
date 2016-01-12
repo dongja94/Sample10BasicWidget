063f03289133be12dd18557574ca86a233668440
dongja94 <dongja94@gmail.com>
2016-01-12 11:04:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dongja94/samplebasicwidget/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.dongja94.samplebasicwidget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView messageView;
+
+    int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        messageView = (TextView)findViewById(R.id.text_message);
+        Button btn = (Button)findViewById(R.id.btn_change);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                String message = getResources().getString(R.string.text_test);
+//                messageView.setText(message);
+//                messageView.setText(R.string.text_test);
+                count = 5;
+                messageView.setText(""+count);
+//                String message = getResources().getString(R.string.text_html);
+//                messageView.setText(Html.fromHtml(message));
+            }
+        });
+    }
+}
